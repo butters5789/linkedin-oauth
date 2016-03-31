67e3c08bcff20405a4620054ccc3423d49a1c056
Brad Butterfield <butters5789@gmail.com>
2016-03-31 15:40:53 -0600

    user available everywhere, user can logout

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  console.log(req.user);
   res.render('index', {
-    title: 'Express',
-    user: req.user
+    title: 'Express'
   });
+});
+
+router.get('/logout', function(req, res, next) {
+  res.clearCookie('session');
+  res.redirect('/');
 });
 
 module.exports = router;
